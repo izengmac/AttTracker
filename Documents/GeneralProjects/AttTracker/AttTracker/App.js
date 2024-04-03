e7658577d141ddfa09596cff5a086c816c38ae8f
macdantat <macdanieltatenda00@gmail.com>
2024-04-03 04:26:05 +0300

    Onboarding screen and loginscreen

--- Documents/GeneralProjects/AttTracker/AttTracker/App.js
+++ Documents/GeneralProjects/AttTracker/AttTracker/App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import OnboardingScreen from './screens/OnboardingScreen';
+import LoginScreen from './screens/LoginScreen';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <LoginScreen/>
+  )
+    
 }
 
 const styles = StyleSheet.create({
